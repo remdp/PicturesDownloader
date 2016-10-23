f467eaf02f8ae36b1fc7adfe785458b2af607dfd
рома <remmdp@gmail.com>
2016-10-23 13:49:11 +0300

    удален класс NewThread

---
+++ src/ImagesLoader.java
@@ -0,0 +1,74 @@
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Observable;
+import java.util.Observer;
+
+/**
+ * Created by рома on 22.10.2016.
+ */
+public class ImagesLoader  implements Observer{
+
+    public void update (Observable obj, Object arg){
+        System.out.println("Загружена картинка: " + arg);
+    }
+
+    public void loadImages(String[] urls) {
+        String folder = "d:/";
+        String extension = ".jpg";
+        for (int i = 0; i < urls.length; i++) {
+
+            class ImageLoader extends Observable implements Runnable {
+
+                String urlS;
+                String fileName;
+
+                ImageLoader(String urlS, String fileName, ImagesLoader imagesLoader) {
+                    this.addObserver(imagesLoader);
+                    this.urlS = urlS;
+                    this.fileName = fileName;
+                    new Thread(this).start();
+                }
+
+                private void LoadImage(String urlS, String fileName) {
+
+                    try {
+                        URL url = new URL(urlS);
+                        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                        urlConnection.connect();
+                        InputStream inputStream = urlConnection.getInputStream();
+
+                        byte[] buffer = new byte[1024];
+                        int bufferLength = 0;
+
+                        File file = new File(folder + fileName + extension);
+                        FileOutputStream fos = new FileOutputStream(file);
+
+                        while ((bufferLength = inputStream.read(buffer)) > 0) {
+                            fos.write(buffer, 0, bufferLength);
+                        }
+
+                        fos.close();
+                        inputStream.close();
+
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+
+                public void run(){
+                   LoadImage(urlS,fileName);
+                    setChanged();
+                    notifyObservers(folder + fileName + extension);
+                }
+            }
+            ImageLoader imageLoader = new ImageLoader(urls[i],"File" + i, this);
+        }
+
+     //   System.out.println("Загрузка картинок завершена!" );
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import java.util.Observable;
+import java.util.Observer;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        String[] pathes = new String[5];
+        pathes[0] = "http://minionomaniya.ru/wp-content/uploads/2016/01/%D0%9A%D0%B5%D0%B2%D0%B8%D0%BD.jpg";
+        pathes[1] = "http://minionomaniya.ru/wp-content/uploads/2016/03/Minions-foto-iz-filma.jpg";
+        pathes[2] = "http://minionomaniya.ru/wp-content/uploads/2015/10/миньоны-мультфильм-картинки.jpg";
+        pathes[3] = "http://minionomaniya.ru/wp-content/uploads/2015/10/картинки-из-мультфильма-миньоны-2015.jpg";
+        pathes[4] = "http://minionomaniya.ru/wp-content/uploads/2015/08/Миньон-Боб.jpg";
+
+        ImagesLoader imagesLoader = new ImagesLoader();
+        imagesLoader.loadImages(pathes);
+
+    }
+}
